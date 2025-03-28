de77420e4847e2ab10d2218726a749dc5863a654
SAINT-RONIN <731427@student.inholland.nl>
2025-03-28 15:36:12 +0100

    Major Update incoming

--- Someren/Repositories/IStudentRepository.cs
+++ Someren/Repositories/IStudentRepository.cs
@@ -1,16 +1,1 @@
-﻿using System.Collections.Generic;
-using Someren.Models;
-
-namespace Someren.Repositories
-{
-    public interface IStudentRepository
-    {
-        List<Student> GetAllStudents();
-        Student? GetStudentByID(int id);
-        void AddStudent(Student student);
-        void UpdateStudent(Student student);
-        void DeleteStudent(Student student);
-        List<Student> GetStudentsByRoomID(int roomId);
-        List<Student> GetStudentsByLastName(string lastName);
-    }
-}
+﻿
